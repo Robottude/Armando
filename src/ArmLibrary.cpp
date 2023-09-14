66b6b4c64f171778c8f85e3cbb0103a965b45a4c
碧 / Aoto <145064328+aoto959@users.noreply.github.com>
2023-09-14 22:39:48 +0200

    Add files via upload

---
+++ src/ArmLibrary.cpp
@@ -0,0 +1,15 @@
+// UNIVERSAL ROBOT ARM LIBRARY // ユニバーサルロボットアームライブラリ
+
+#include "ArmLibrary.h"
+
+// stepper class implem
+
+// ... StepperMotor -> SettingDirection,Steps
+
+// robot class implem
+
+// ... Robot -> DH Params, Move Joint, Move to Position 
+// 
+
+//
+
---
+++ src/ArmLibrary.h
@@ -0,0 +1,12 @@
+// UNIVERSAL ROBOT ARM LIBRARY // ユニバーサルロボットアームライブラリ
+
+#ifndef UNIVERSAL_ROBOT_MANIPULATOR_H
+#define UNIVERSAL_ROBOT_MANIPULATOR_H
+
+// Necessary Libraries 
+
+// DH structure, Robot Structure, Trans Matrix
+
+// Motor Classes, Robot Classes (Forward&Inverse Kin, TransMat calculate) etc.
+
+#endif // UNIVERSAL_ROBOT_MANIPULATOR_H
