337856e2a22c4ae9ca9063a2ea7acea746e98b07
vla-gove <107414426+vla-gove@users.noreply.github.com>
2023-09-14 22:50:16 +0200

    Added prototypes

--- src/ArmLibrary.h
+++ src/ArmLibrary.h
@@ -6,7 +6,38 @@
 // Necessary Libraries 
 
 // DH structure, Robot Structure, Trans Matrix
+struct DHParameters {
+    double theta;
+    double d;
+    double a;
+    double alpha;
+};
+
+struct RobotConfiguration {
+    // dof size (configurable?)
+};
+
+struct TransformationMatrix {
+    double TransMatrix[4][4];
+};
 
 // Motor Classes, Robot Classes (Forward&Inverse Kin, TransMat calculate) etc.
+
+class StepperMotor {
+private:
+    // pins
+
+public:
+   // move / set direction
+};
+
+class RobotManipulator {
+private:
+    // fk, ik, and trans matrix calculation
+
+public:
+    // move joint / move to position(ik)
+};
+
 
 #endif // UNIVERSAL_ROBOT_MANIPULATOR_H
