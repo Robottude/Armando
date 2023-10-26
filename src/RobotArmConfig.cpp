a5278a468a44999a9477ad7ab107ebd55f709f14
vla-gove <107414426+vla-gove@users.noreply.github.com>
2023-10-26 19:25:25 +0200

    Added getters and setters for Robot parameters

--- src/RobotArmConfig.cpp
+++ src/RobotArmConfig.cpp
@@ -1,6 +1,33 @@
 // RobotArmConfig.cpp
 #include "RobotArmConfig.h"
 
-RobotArmConfig::RobotArmConfig() {
-  // initialize DH parameters here
+// global variables for robot parameters
+LinkLengths link_lengths;
+EndEffectorPosition end_effector_position;
+JointAngles joint_angles;
+
+// setters
+void initializeLinkLengths(const LinkLengths& lengths) {
+    link_lengths = lengths;
+}
+
+void initializeEndEffectorPosition(const EndEffectorPosition& position) {
+    end_effector_position = position;
+}
+
+void initializeJointAngles(const JointAngles& angles) {
+    joint_angles = angles;
+}
+
+// getters
+LinkLengths getLinkLengths() {
+    return link_lengths;
+}
+
+EndEffectorPosition getEndEffectorPosition() {
+    return end_effector_position;
+}
+
+JointAngles getJointAngles() {
+    return joint_angles;
 }
