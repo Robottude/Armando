eb4db8a23f5160632584f15bc2de3afbaa6a6344
vla-gove <107414426+vla-gove@users.noreply.github.com>
2023-11-01 21:58:12 +0100

    Improved kinematic equations, added angle values conversion

--- Armed/InverseKinematics.cpp
+++ Armed/InverseKinematics.cpp
@@ -2,30 +2,39 @@
 #include "InverseKinematics.h"
 #include <math.h>
 
+// inverse kinematics calculates necessary joint values to achieve given end effector's position in space (x, y, z)
+
 JointAngles calculateInverseKinematics(const LinkLengths& linkLengths, const EndEffectorPosition& position) {
-	JointAngles angles;
+    JointAngles angles;
 
-	// end effector position
-	double x = position.x;
-	double y = position.y;
-	double z = position.z;
+    double x = position.x;
+    double y = position.y;
+    double z = position.z;
 
-	// basic trigonometry
-	angles.theta1 = atan2(y, x);
+    // rotation around the z-axis
+    angles.theta1 = atan2(y, x);
 
-	// the remaining lengths in the X-Y plane
-	double L = sqrt(x * x + y * y);
-	double D = sqrt(L * L + z * z);
+    // the distance between joint 1 and the end effector in the xy plane
+    double r_xy = sqrt(x * x + y * y);
 
-	// law of cosines
-	double cosTheta3 = (linkLengths.a1 * linkLengths.a1 + linkLengths.a2 * linkLengths.a2 - D * D) / (2 * linkLengths.a1 * linkLengths.a2);
-	angles.theta3 = acos(cosTheta3);
+    // the distance between joint 1 and the end effector in space
+    double r = sqrt(r_xy * r_xy + z * z);
 
-	double sinTheta3 = sin(angles.theta3);
-	double theta = atan2(z, L);
-	double cosTheta2 = (L * L + z * z + linkLengths.a1 * linkLengths.a1 - linkLengths.a2 * linkLengths.a2) / (2 * L * linkLengths.a1);
+    // joint 3 sine and cosine
+    double cosTheta3 = (linkLengths.a2 * linkLengths.a2 + linkLengths.a3 * linkLengths.a3 - r * r) / (2 * linkLengths.a2 * linkLengths.a3);
+    double sinTheta3 = -sqrt(1 - cosTheta3 * cosTheta3); // Use negative sign for this configuration
 
-	angles.theta2 = theta - atan2(sinTheta3, cosTheta2);
+    // joint 3 value
+    angles.theta3 = atan2(sinTheta3, cosTheta3);
 
-	return angles;
+    // intermediate angle for joint 2 calculation
+    double alpha = atan2(z, r_xy);
+
+    // joint 2 cosine
+    double cosTheta2 = (r * r + linkLengths.a2 * linkLengths.a2 - linkLengths.a3 * linkLengths.a3) / (2 * r * linkLengths.a2);
+
+    // joint 2 value
+    angles.theta2 = alpha - atan2(sqrt(1 - cosTheta2 * cosTheta2), cosTheta2);
+
+    return angles;
 }
